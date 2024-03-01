0d104a87c7fb0bbea9521a88ea4ac6acdb0f3f36
Spencer Boggs <spencer_boggs@icloud.com>
2024-02-29 19:18:47 -0800

    Updated Invite Validation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os, json, re, requests, time
-from datetime import datetime
 
 def __main__():
+    # Load user data
     user_info = get_user_info()
+
+    # Title display
     print()
     os.system('cls' if os.name == 'nt' else 'clear')
     print("██████╗░██╗░██████╗░█████╗░░█████╗░██████╗░██████╗░  ██████╗░░█████╗░████████╗░█████╗░")
@@ -19,6 +21,8 @@ def __main__():
     print("██████╔╝╚█████╔╝██║░░██║░░░██║░░░███████╗██║░░██║")
     print("╚═════╝░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝")
     print("By Spencer Boggs\n")
+
+    # Main menu
     print("\033[1mMain Menu\033[0m")
     print(f"User: {user_info[0]}")
     print(f"Email: {user_info[1]}")
@@ -27,13 +31,14 @@ def __main__():
     print(f"Phone: {user_info[4]}")
     print("")
     
+    # Load server and invite data
     print("Loading data...")
     servers = get_servers()
     invites = get_server_invites()
     valid_invites = []
     print("Data loaded\n")
 
-
+    # Main menu loop
     while True:
         print("1. Get servers")
         print("2. Get all invites")
@@ -43,12 +48,14 @@ def __main__():
         print("6. Exit")
         choice = input("Enter a choice: ")
 
+        # Get all servers
         if choice == '1':
             print("\nIn order of creation: ")
             print("\n".join(servers))
             print(f"\nFound {len(servers)} servers you are in")
             print("")
 
+        # Get all invites
         elif choice == '2':
             print("\nAll server invites you have sent: ")
             for i in range(0, len(invites), 10):
@@ -57,18 +64,19 @@ def __main__():
             print(f"\nFound {len(invites)} invites sent by you")
             print("")
 
+        # Get valid invites
         elif choice == '3':
-            print("\nChecking for valid invites...")
-            print(f"Estimated time: {len(invites) * 2} seconds\n")
-            if valid_invites == []:
-                valid_invites = get_working_invites()
-            print("\nValid invites: ")
+            print("\nChecking for valid invites...\n")
+            valid_invites = get_working_invites()
+
             if valid_invites != []:
+                print("\nValid invites: ")
                 for i in range(0, len(valid_invites), 10):
                     print(f"\nDisplaying invites {i+1} to {min(i+10, len(valid_invites))} of {len(valid_invites)}:")
                     print("\n".join(valid_invites[i:i+10]))
                 print(f"\nFound {len(valid_invites)} valid invites sent by you")
 
+        # Search messages
         elif choice == '4':
             print()
             messages = search_messages_menu()
@@ -76,29 +84,40 @@ def __main__():
             print(f"\nFound {len(messages)} messages sent by you")
             print("")
 
+        # Get total messages
         elif choice == '5':
             print(f"\nTotal messages sent by you in this data package: {total_messages()}")
             print("")
 
+        # Exit
         elif choice == '6':
             print()
             os.system('cls' if os.name == 'nt' else 'clear')
             break
 
+        # Invalid choice
         else:
             print("Invalid choice")
+
+        # Await user input to continue
         input("Press enter to continue...")
         print()
         os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def get_user_info():
+    """ 
+        Returns the user's global name, email, id, ip, and phone number
+    """
     with open('package/account/user.json', 'r') as file:
         data = json.load(file)
         return data['global_name'], data['email'], data['id'], data['ip'], data['phone']
 
 
 def get_servers():
+    """ 
+        Returns a list of servers the user is in
+    """
     servers = []
     for server in os.listdir('package/servers'):
         if os.path.isdir(f'package/servers/{server}'):
@@ -111,6 +130,9 @@ def get_servers():
 
 
 def get_server_invites():
+    """
+        Returns a list of all invites the user has sent
+    """
     invites = set()
     for channel_dir in os.listdir('package/messages'):
         channel_path = os.path.join('package/messages', channel_dir)
@@ -127,58 +149,111 @@ def get_server_invites():
     invites_list.sort()
     return invites_list
 
+
 def extract_invite_link(line):
+    """ 
+        Extracts an invite link from a line of text
+    """
     match = re.search(r'https?://(?:www\.)?(?:discord\.gg/[^\s,]+|discord\.com/invite/[^\s,]+)', line)
     if match:
         return match.group(0).rstrip(',').rstrip('"')
     return None
 
+
 def is_valid_invite(invite_link):
+    """ 
+        Checks if an invite link is valid
+    """
     return invite_link is not None
 
 
 def get_invite_codes():
+    """ 
+        Returns a list of invite codes
+    """
     full_links = get_server_invites()
     return [link.split('/')[-1] for link in full_links]
 
 
 def get_working_invites():
+    """
+        Returns a list of valid invite links
+    """
     code_list = get_invite_codes()
+    if os.path.exists('invites.json'):
+        with open('invites.json', 'r') as file:
+            data = json.load(file)
+            if data['all']:
+                print("All invites have been checked. Skipping to the end.")
+                return [f"https://discord.gg/{code}" for code in data['valid']]
+            
+            elif data['latest'] in code_list:
+                print(f"Skipping to the latest checked invite: {data['latest']}")
+                code_list = code_list[code_list.index(data['latest'])+1:]
+                print(f"Valid codes found so far: ")
+                print("\n".join([f"https://discord.gg/{code}" for code in data['valid']]), "\n")
+                print(f"Remaining invites to check: {len(code_list)}")
+    else:
+        with open('invites.json', 'w') as file:
+            json.dump({"valid": [], "invalid": [], "latest": "", "all": False}, file)
+
     valid_codes = []
+    valid_links = []
 
     print("Notes:\n - The valid invites found are sometimes buggy. \n - Sometimes they claim to be invalid but are actually valid. \n - Try codes in your browser and not on discord. \n - This process is slow due to discord API rate limits.\n")
 
     print(f"Estimated time: {len(code_list) * 2} seconds\n")
 
     for invite_code in code_list:
+
         api_endpoint = f"https://discord.com/api/v9/invites/{invite_code}"
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
             "Content-Type": "application/json",
         }
 
         response = requests.get(api_endpoint, headers=headers)
+
         if response.status_code == 429:
             print("Rate limited. Try again later.")
             print("Wait at least 10 minutes. Discord api rate limits are strict.")
-            return []
-        if response.status_code == 200:
-            valid_codes.append("https://discord.gg/" + invite_code)
-        time.sleep(1)
-    return valid_codes
+            print("The valid codes found so far are saved. You can continue from where you left off later.\n")
 
+            if len(valid_codes) > len(data['valid']):
+                with open('invites.json', 'w') as file:
+                    json.dump({"valid": valid_codes, "latest": invite_code, "all": False}, file)
+
+            return []
+        
+        if response.status_code == 200:
+            valid_codes.append(invite_code)
+            valid_links.append("https://discord.gg/" + invite_code)
+            print("Valid invite found: https://discord.gg/" + invite_code)
+
+        time.sleep(1.2)
+
+    with open('invites.json', 'w') as file:
+        json.dump({"valid": valid_codes, "latest": "", "all": True}, file)
+
+    return valid_links
 
 
 def search_messages_menu():
+    """ 
+        Prompts the user for a search term and returns a list of messages containing the search term
+    """
     print()
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("Notes: \n - Searching for numbers also returns dates and times. \n - Searching for non alphanumeric characters may return unexpected results. \n - Results with over 1000 messages may be truncated.\n\t(If you use VScode the max lines is 1000, cmd is 9999) \n - Some messages may be missing due to impropper csv formatting.\n\t(Blame discord, not me. I ain't trying to work around that shit) \n")
+    print("Notes: \n - Searching for numbers also returns dates and times as well as message IDs. \n - Searching for non alphanumeric characters may return unexpected results. \n - Results with over 1000 messages may be truncated.\n\t(If you use VScode the max lines is 1000, cmd is 9999) \n - Some messages may be missing due to impropper csv formatting.\n\t(Blame discord, not me. I ain't trying to work around that shit) \n")
     search_term = input('Enter a search term: ')
     messages = search_messages(search_term)
     return messages
 
 
 def search_messages(search_term):
+    """ 
+        Searches through the messages folder for all messages containing search_term  
+    """
     if not search_term:
         print("No search term entered")
         return []
@@ -204,6 +279,9 @@ def search_messages(search_term):
 
 
 def total_messages():
+    """ 
+        Returns the total number of messages in the messages folder
+    """
     total = 0
     for channel_dir in os.listdir('package/messages'):
         channel_path = os.path.join('package/messages', channel_dir)
@@ -214,6 +292,7 @@ def total_messages():
                     with open(file_path, 'r', encoding='utf-8') as file:
                         total += sum(1 for line in file)
     return total
+
 
 if __name__ == '__main__':
     __main__()
